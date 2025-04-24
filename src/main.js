22a5f33db499b691efda65616e7dc8da0813638c
FernandoCoff <fernandocoff@outlook.com>
2025-04-24 18:14:05 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const mult = (a, b) => a * b;
+const hi = (name) => 'Hi, ' + name;
+console.log(mult(2, 5));
+console.log(hi('Luiz'));
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const mult = (a : number, b : number) : number => a*b
+const hi = (name : string) => 'Hi, '+ name
+
+console.log(mult(2,5))
+console.log(hi('Luiz'))
