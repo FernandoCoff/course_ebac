36632257bd54026790c7114285c5f9edb68a3e03
FernandoCoff <fernandocoff@outlook.com>
2025-08-05 16:17:02 -0300

    fix errors

--- cypress/e2e/contact/home.cy.js
+++ cypress/e2e/contact/home.cy.js
@@ -1,21 +1,28 @@
 /// <reference types="cypress" />
 
-describe('Testes para a Home', () =>{
-    it('deve adicionar um contato', ()=>{
+describe('Testes para a Agenda de Contatos', () => {
+    beforeEach(() => {
         cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
-        cy.get('input[type=text]').type('Luiz')
-        cy.get('input[type=email]').type('Luiz@example.com')
-        cy.get('input[type=tel]').type('33999999999')
-        cy.get('button[type=submit]').click()
     })
-    it('deve editar um conato', ()=> {
-        cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
-        cy.contains('div.contato','Luiz@example.com').parent().find('button.edit').click()
-        cy.get('input[type=text]').type('Luiz Fernando')
+
+    it('deve adicionar um contato', () => {
+        cy.get('input[type="text"]').type('Luiz')
+        cy.get('input[type="email"]').type('luiz@example.com')
+        cy.get('input[type="tel"]').type('33999999999')
+        cy.get('button[type="submit"]').click()
+        cy.contains('.contato', 'luiz@example.com').should('be.visible')
+    })
+
+    it('deve editar um contato', () => {
+        cy.contains('.contato', 'luiz@example.com').find('.edit').click()
+        cy.get('input[type="text"]').clear().type('Luiz Fernando')
+        cy.get('input[type="email"]').clear().type('luiz.fernando@example.com')
         cy.get('.alterar').click()
+        cy.contains('.contato', 'Luiz Fernando').should('be.visible')
     })
-    it('deve excluir um conato', ()=> {
-        cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
-        cy.contains('div.contato','Luiz@example.com').parent().find('button.delete').click()
+
+    it('deve excluir um conato', () => {
+        cy.contains('.contato', 'luiz.fernando@example.com').find('.delete').click()
+        cy.contains('.contato', 'luiz.fernando@example.com').should('not.exist')
     })
 })
